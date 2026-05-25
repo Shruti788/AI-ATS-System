5d82a625b8cf841aeb004195790be2894ba310a1
Shruti Yadav <shrutiycs78@gmail.com>
2026-05-25 13:42:59 +0530

    AI ATS deployed backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
-// attach token automatically (for future auth)
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
+  baseURL: "https://ai-ats-system.onrender.com/api",
 });
 
 export default API;
